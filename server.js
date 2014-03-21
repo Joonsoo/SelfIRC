fada78aae85754da0e10eeafd41962e2a0c7eb26
Joonsoo Jeon <giyeok7@gmail.com>
2014-03-21 14:44:31 +0000

    Many things added   - init text set on server   - message timestamp records on server   - date format changed   - pingpong logging option added   - timezone option added   - cookie secret option added   - /save and /clear commands now return the result

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.log('This process is pid ' + process.pid);
 
 var net = require("net");
 var fs = require("fs");
@@ -9,9 +10,15 @@ var io = require("socket.io");
 var config = require("./config.js");
 
 console.log(config);
+var generalOpt = config.generalOpt;
 var ircOpt = config.ircOpt;
 var httpOpt = config.httpOpt;
 var sqlOpt = config.sqlOpt;
+
+if (generalOpt.timezone) {
+    process.env.TZ = generalOpt.timezone;
+    console.log(new Date().toString());
+}
 
 var logs = [];
 var lastServerMsg = null;
@@ -21,12 +28,16 @@ var ircSeparator = "\n\r";
 
 var logId = 0;
 function newLogItem(type, msg) {
-    var newlog = {type: type, id: logId++, msg: msg};
-    logs.push(newlog);
+    var newlog = {type: type, id: logId++, msg: msg, timestamp: new Date().getTime()};
+    var isAutoPingPong = ((type === "sc" && msg.substring(0, 4) === "PING") || (type === "cs" && msg.substring(0, 4) === "PONG"));
+    if (generalOpt.logPingPong || !isAutoPingPong) {
+        logs.push(newlog);
+    }
     return newlog;
 }
 
 var client;
+var fixedTyping = "";
 function write(msg) {
     client.write(msg);
     client.write(ircSeparator);
@@ -88,7 +99,7 @@ function processMsg(log) {
 
 var app = express();
 
-app.use(express.cookieParser('xlzkcvj'));
+app.use(express.cookieParser(generalOpt.cookieSecret));
 app.use(express.cookieSession());
 
 app.get("/", function(req, res) {
@@ -113,6 +124,9 @@ iolisten.sockets.on('connection', function(socket) {
     } else {
         socket.emit("log", logs);
     }
+    if (fixedTyping) {
+        socket.emit("fixedTyping", fixedTyping);
+    }
     socket.on("send", function(data) {
         var lowerCased = data.toLowerCase().trim();
         if (lowerCased === "/restart") {
@@ -121,12 +135,22 @@ iolisten.sockets.on('connection', function(socket) {
             client = null;
             startIRC();
         } else if (lowerCased === "/save") {
-            var filename = saveLog();
-            broadcastNewLogs([newLogItem("log", "/save to " + filename)]);
+            saveLog(function (filename, err) {
+                if (err) {
+                    broadcastNewLogs([newLogItem("log", "/error while saving to " + filename + " -- " + JSON.stringify(err))]);
+                } else {
+                    broadcastNewLogs([newLogItem("log", "/saved to " + filename)]);
+                }
+            });
         } else if (lowerCased === "/clear") {
-            var filename = saveLog();
-            logs = [];
-            broadcastNewLogs([newLogItem("log", "/clear logs after save logs to " + filename)]);
+            saveLog(function (filename, err) {
+                if (err) {
+                    broadcastNewLogs([newLogItem("log", "/error while saving to " + filename + " -- " + JSON.stringify(err))]);
+                } else {
+                    logs = [];
+                    broadcastNewLogs([newLogItem("log", "/clear logs after save logs to " + filename)]);
+                }
+            });
         } else {
             write(data);
         }
@@ -136,15 +160,26 @@ iolisten.sockets.on('connection', function(socket) {
         var start = oldest - 50;
         socket.emit("older", _.filter(logs, function(log) { return log.id >= start && log.id < oldest; }));
     });
+    socket.on("fixedTyping", function (data) {
+        fixedTyping = data;
+        _.each(broadcasting, function (socket) {
+            socket.emit("fixedTyping", fixedTyping);
+        });
+    });
     socket.on("disconnect", function () {
         broadcasting.splice(broadcasting.indexOf(socket));
     });
 });
 
-var saveLog = function () {
+var saveLog = function (donecb) {
     console.log("Saving logs...");
-    var filename = "./logs/SelfIRC-logs-" + (new Date().getTime()) + ".txt";
-    fs.writeFile(filename, JSON.stringify(logs));
+    var filename = "./logs/SelfIRC-logs-" + (new Date().toString()) + ".txt";
+    fs.writeFile(filename, JSON.stringify(logs), function(err) {
+        console.log(err);
+        if (donecb) {
+            donecb(filename, err);
+        }
+    });
     return filename;
 };
 setInterval(saveLog, 8*60*60*1000);		// every 8 hours
